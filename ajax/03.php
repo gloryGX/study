21b99f48ec5ef1be137e6c3b20d5370c8cfe3661
gloryGX <1724135228@qq.com>
2020-01-02 17:51:16 +0800

    这是分支上的第一次提交

--- ajax/03.php
+++ ajax/03.php
@@ -18,6 +18,14 @@ $arr=$dao->fetchAll($sql);
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        table{
+            width: 400px;
+            margin: 0 auto;
+            border: 1px solid black;
+            background-color:red;
+        }
+    </style>
 </head>
 <body>
     <table>
